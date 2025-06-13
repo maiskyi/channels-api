8987e0137ca79e4114ab31830ae746e68a9e5b58
Andrii Maiskyi <andrey@wearehelful.com>
2025-06-13 16:09:39 +0100

    feat: start. #4

--- src/_bot/updates/start/start.update.ts
+++ src/_bot/updates/start/start.update.ts
@@ -1,9 +1,34 @@
+import { Logger } from '@nestjs/common';
 import { Update, Start, Ctx, Context } from '@services/tg-bot';
 
 @Update()
 export class StartUpdate {
+  private logger = new Logger(StartUpdate.name, {
+    timestamp: true,
+  });
+
   @Start()
   public async start(@Ctx() ctx: Context) {
-    await ctx.reply('123');
+    this.logger.log(ctx.from);
+    try {
+      await ctx.reply('123', {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: 'Subscribe to a channel',
+                url: 'https://google.com',
+              },
+            ],
+            [
+              { text: '👍 Yes', callback_data: 'yes' },
+              { text: '👎 No', callback_data: 'no' },
+            ],
+          ],
+        },
+      });
+    } catch (error) {
+      this.logger.error(error);
+    }
   }
 }
